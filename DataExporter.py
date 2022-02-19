fe6ee68c7735760960c55681258ca65e90f8a324
Shion Ichikawa <shion1305@gmail.com>
2022-02-20 06:45:51 +0900

    [FIX] removed concurrent function

--- DataExporter.py
+++ DataExporter.py
@@ -1,7 +1,9 @@
 import json
+import time
 from concurrent import futures
 
 from elasticsearch import Elasticsearch
+
 
 def exportJson(target, year, month, day):
     json_string = json.dumps(target)
@@ -10,7 +12,7 @@ def exportJson(target, year, month, day):
     print('COMPLETED: ImportedData/data20%02d-%02d%02d.json' % (year, month, day))
 
 
-def exportDaily(year, month, day, out):
+def exportDaily(year, month, day):
     es = Elasticsearch("http://localhost:12333")
     result = []
     for j in range(0, 24):
@@ -25,40 +27,36 @@ def exportDaily(year, month, day, out):
                     }
                 }
             }
+            print(query)
             r1 = es.search(index="xpot_accesslog-20%02d.%02d" % (year, month), body=query,
                            size=10000)['hits']['hits']
+            print(len(r1))
             if len(r1) == 10000: print("WARNING 20%02d-%02d-%02dT%02d:%02d:00.000Z" % (year, month, day, j, k * 10))
             result.extend(r1)
     # exportJson(target=result, year=year, month=month, day=day)
-    out.submit(exportJson, target=result, year=year, month=month, day=day)
+    exportJson(target=result, year=year, month=month, day=day)
 
 
-def exportAllData(start=2110, end=2112):
-    from elasticsearch import Elasticsearch
-    t = [int(str(start)[:2]), int(str(start)[2:])]
-    e = [int(str(end)[:2]), int(str(end)[2:])]
-    result = []
-    with futures.ThreadPoolExecutor(max_workers=2) as out_exec:
-        with futures.ThreadPoolExecutor(max_workers=2) as executor:
-            while t[0] * 16 + t[1] <= e[0] * 16 + e[1]:
-                future_list = []
-                for i in range(1, getDays_in_Month(t[0] * 100 + t[1]) + 1):
-                    future = executor.submit(exportDaily, year=t[0], month=t[1], day=i, out=out_exec)
-                    future_list.append(future)
-            if t[1] == 12:
-                t[1] = 1
-                t[0] += 1
-            else:
-                t[1] += 1
-        _ = futures.as_completed(fs=future_list)
-        out_exec.shutdown()
+def exportAllData(start=211001, end=211231):
+    t = [int(str(start)[:2]), int(str(start)[2:4]), start % 100]
+    e = [int(str(end)[:2]), int(str(end)[2:4]), end % 100]
+    while t[0] * 16 + t[1] <= e[0] * 16 + e[1]:
+        for i in range(t[2] if int(str(start)[:2]) == t[0] and int(str(start)[2:4]) == t[1] else 1,
+                       e[2] + 1 if t[0] == e[0] and t[1] == e[1] else getDays_in_Month(t[0] * 100 + t[1]) + 1):
+            exportDaily(year=t[0], month=t[1], day=i)
+            time.sleep(30)
+        if t[1] == 12:
+            t[1] = 1
+            t[0] += 1
+        else:
+            t[1] += 1
 
 
 def getDays_in_Month(t):
     d = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    if (t % 20 == 2):
+    if t % 20 == 2:
         return 29
     return d[t % 20 - 1]
 
 
-exportAllData()
+exportAllData(start=211114, end=211231)
