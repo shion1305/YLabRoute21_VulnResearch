110776361ff22d1865a0e5a40640b4bf68e23c72
Shion Ichikawa <shion1305@gmail.com>
2022-02-20 03:32:56 +0900

    [IMPROVE] use threading in saving data

--- DataCollector.py
+++ DataCollector.py
@@ -14,7 +14,14 @@ def importNotDetectData(start=2101, end=2201):
     return search(query, start, end)
 
 
-def export(year, month, day):
+def exportJson(target, year, month, day):
+    json_string = json.dumps(target)
+    with open('ImportedData/data20%02d-%02d%02d.json' % (year, month, day), 'w') as outfile:
+        json.dump(json_string, outfile)
+    print('COMPLETED: ImportedData/data20%02d-%02d%02d.json' % (year, month, day))
+
+
+def exportDaily(year, month, day, out):
     es = Elasticsearch("http://localhost:12333")
     result = []
     for j in range(0, 24):
@@ -33,10 +40,8 @@ def export(year, month, day):
                            size=10000)['hits']['hits']
             if len(r1) == 10000: print("WARNING 20%02d-%02d-%02dT%02d:%02d:00.000Z" % (year, month, day, j, k * 10))
             result.extend(r1)
-    json_string = json.dumps(result)
-    with open('ImportedData/data20%02d-%02d%02d.json' % (year, month, day), 'w') as outfile:
-        json.dump(json_string, outfile)
-    print('COMPLETED: ImportedData/data20%02d-%02d%02d.json' % (year, month, day))
+    # exportJson(target=result, year=year, month=month, day=day)
+    out.submit(exportJson, target=result, year=year, month=month, day=day)
 
 
 def exportAllData(start=2110, end=2112):
@@ -44,18 +49,20 @@ def exportAllData(start=2110, end=2112):
     t = [int(str(start)[:2]), int(str(start)[2:])]
     e = [int(str(end)[:2]), int(str(end)[2:])]
     result = []
-    with futures.ThreadPoolExecutor(max_workers=4) as executor:
-        while t[0] * 16 + t[1] <= e[0] * 16 + e[1]:
-            future_list = []
-            for i in range(1, getDays_in_Month(t[0] * 100 + t[1]) + 1):
-                future = executor.submit(export, year=t[0], month=t[1], day=i)
-                future_list.append(future)
-        if t[1] == 12:
-            t[1] = 1
-            t[0] += 1
-        else:
-            t[1] += 1
-    _ = futures.as_completed(fs=future_list)
+    with futures.ThreadPoolExecutor(max_workers=2) as out_exec:
+        with futures.ThreadPoolExecutor(max_workers=2) as executor:
+            while t[0] * 16 + t[1] <= e[0] * 16 + e[1]:
+                future_list = []
+                for i in range(1, getDays_in_Month(t[0] * 100 + t[1]) + 1):
+                    future = executor.submit(exportDaily, year=t[0], month=t[1], day=i, out=out_exec)
+                    future_list.append(future)
+            if t[1] == 12:
+                t[1] = 1
+                t[0] += 1
+            else:
+                t[1] += 1
+        _ = futures.as_completed(fs=future_list)
+        out_exec.shutdown()
 
 
 def getDays_in_Month(t):
